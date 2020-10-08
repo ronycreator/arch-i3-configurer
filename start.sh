98a760fb6da0dffb5a474d0b26cbbe35155182f3
Genghius <62076069+Genghius@users.noreply.github.com>
2020-10-08 01:21:35 +0100

    Updated start.sh and made it so that this shit onl
    y takes 1 user for stuff

--- start.sh
+++ start.sh
@@ -17,7 +17,7 @@ pulseaudio --version 2> /dev/null 1> /dev/null || (echo "pulseaudio $yikes" && s
 echo "checking i3 or i3-gaps is installed..."
 i3 -v 2> /dev/null 1> /dev/null || (echo "i3-gaps $yikes" && sudo pacman -S --noconfirm i3-gaps ttf-dejavu 1> /dev/null 2> /dev/null) 
 
-user=$(who | awk '{print $1}')
+user=$(who -m | awk '{print $1}')
 echo "proceeding to create directories, copy and overwrite dotfiles"
 ls /usr/share/i3blocks/ 2> /dev/null 1> /dev/null || sudo mkdir /usr/share/i3blocks
 ls /home/$user/.config/i3 2> /dev/null 1> /dev/null || mkdir /home/$user/.config/i3
